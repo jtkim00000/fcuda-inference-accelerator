dd45ec94ef226107165b9e3ebc9769d13eeaec14
jtkim00000 <jessetkim00000@gmail.com>
2026-08-12 17:30:40 -0700

    more profiling results

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <chrono>
 #include <cuda_runtime.h>   
+#include <cstring>
 
 #include "params/read_params.h"
 #include "math/activation_compressor.h"
@@ -32,7 +33,7 @@ int main()
 
     const int num_iterations = 100000;
 
-    const int num_act = 1;
+    const int num_act = 1024;
 
     std::cout << "Initializing Parameters . . . " << std::endl;
 
@@ -124,7 +125,16 @@ int main()
 
         auto t_loop_start = std::chrono::high_resolution_clock::now();
 
-        X = load_bin("../input/test_case.bin", 784 * num_act);
+        int half_size = (784 * num_act) / 2;
+
+        float* X_half = load_bin("../input/test_case.bin", half_size);
+
+        X = new float[2 * half_size];
+
+        std::memcpy(X, X_half, half_size * sizeof(float));
+        std::memcpy(X + half_size, X_half, half_size * sizeof(float));
+
+        delete[] X_half;
 
         auto t4 = std::chrono::high_resolution_clock::now();
 
